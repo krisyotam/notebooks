51f2443d66ad106a357724e7ec31e165e9723e62
Kris Yotam <75515498+krisyotam@users.noreply.github.com>
2026-04-06 13:09:16 -0500

    match header width to footer, replace top cite with back arrow link

--- build.js
+++ build.js
@@ -125,7 +125,10 @@ ${mathjaxBlock()}
 </head>
 <body>
 
-<p></p><cite><a href="/notebooks/">Notebooks</a></cite>
+<a href="/notebooks/" class="nb-back">
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
+  Back to Notebooks
+</a>
 
 <div class="nb-header">
   <h1 class="nb-header-title">${title}</h1>
